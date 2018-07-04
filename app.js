1c6ba3c66943d8bcb5af5a0cbfefd9aed9b13e41
Aldrin Oliveira <aldrin.o.247@gmail.com>
2018-07-04 11:29:25 +0530

    Wiring up Weather

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+
+// 4a04d1c42fd9d32c97a2c291a32d5e2d
+
 const request = require('request');
-const yargs = require('yargs');
-
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
-
-var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+  url: 'https://api.forecast.io/forecast/4a04d1c42fd9d32c97a2c291a32d5e2d/39.9396284,-75.18663959999999',
   json: true
 }, (error, response, body) => {
   if (error) {
-    console.log('Unable to connect to Google servers.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    console.log('Unable to connect to Forecast.io server.');
+  } else if (response.statusCode === 400) {
+    console.log('Unable to fetch weather.');
+  } else if (response.statusCode === 200) {
+    console.log(body.currently.temperature);
   }
 });
